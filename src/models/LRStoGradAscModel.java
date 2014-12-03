c2cede14571b72892e49f54275d894afcd33608a
XSpring <hldo.2012@phdis.smu.edu.sg>
2014-12-03 01:30:04 -0500

    Add stochastic gradient descent

---
+++ src/models/LRStoGradAscModel.java
@@ -0,0 +1,221 @@
+package models;
+
+import controllers.dataControllers.FeatureController;
+import controllers.dataControllers.dataController;
+import utilities.Configuration;
+
+import java.util.List;
+
+/**
+ * Create on 2/12/14
+ *
+ * @author Loc Do
+ */
+public class LRStoGradAscModel extends genericModel {
+
+    public LRStoGradAscModel(List<Object> trainData, List<Object> testData) {
+        // Initialize the data
+        this.trainData = trainData;
+        this.testData = testData;
+
+        // Initialize the parameters
+        modelParams = new FeatureController();
+    }
+
+    public void run() {
+        try {
+            //System.out.println("Training...");
+            train();
+
+            //System.out.println("Testing...");
+            test();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void train() throws Exception {
+        Object[] arr = trainData.toArray();
+
+        for (int idLoop = 0; idLoop < Configuration.getInstance().getNoOfIterations();
+             idLoop++)
+        {
+            // For all pairs (i, j) such that views_i > views_j
+            for (int idI1=0; idI1 < arr.length-1; idI1++)
+                for (int idI2=idI1+1; idI2 < arr.length; idI2++)
+                {
+                    Object item1 = arr[idI1];
+                    Object item2 = arr[idI2];
+
+                    /*
+                    if (dataController.getHmVideo().get(item1).getViewCount() <
+                            dataController.getHmVideo().get(item2).getViewCount())
+                    {
+                        item1 = arr[idI2];
+                        item2 = arr[idI1];
+                    }
+                    */
+
+                    // Create representative feature vector
+                    FeatureController X_ij = new FeatureController();
+
+                    // 1. Numeric features
+                    // 1.0 Intercept weight w_0
+                    X_ij.getHmNumericFeatures().put(0, 1.0);
+
+                    /*
+                    // 1.1 No of likes
+                    X_ij.getHmNumericFeatures().put(1, 1.0*(dataController.getHmVideo().get(item1).getNoOfLikes() /
+                                                            dataController.getHmVideo().get(item2).getNoOfLikes()));
+                    // 1.2 No of dislikes
+                    X_ij.getHmNumericFeatures().put(2, 1.0*(dataController.getHmVideo().get(item1).getNoOfDislikes() /
+                                                            dataController.getHmVideo().get(item2).getNoOfDislikes()));
+                    */
+                    double ratioOfLike1 = dataController.getHmVideo().get(item1).getNoOfLikes();
+                    double ratioOfDislike1 = dataController.getHmVideo().get(item1).getNoOfDislikes();
+
+                    ratioOfLike1 /= (ratioOfLike1 + ratioOfDislike1);
+
+                    double ratioOfLike2 = dataController.getHmVideo().get(item2).getNoOfLikes();
+                    double ratioOfDislike2 = dataController.getHmVideo().get(item2).getNoOfDislikes();
+
+                    ratioOfLike2 /= (ratioOfLike2 + ratioOfDislike2);
+
+                    //X_ij.getHmNumericFeatures().put(1, ratioOfLike1);
+
+                    //X_ij.getHmNumericFeatures().put(2, ratioOfLike2);
+
+                    // 1.3 Video Length In Seconds
+                    //X_ij.getHmNumericFeatures().put(3, 1.0 * dataController.getHmVideo().get(item1).getVideoLengthInSeconds() /
+                    //                                         dataController.getHmVideo().get(item2).getVideoLengthInSeconds());
+
+                    // 2. Bag of Words (from Title only)
+                    String[] titleArr = dataController.getHmVideo().get(item1).getTitle().split(",");
+                    for (String str:titleArr)
+                    {
+                        Double tf = X_ij.getHmBoWFeatures().get(str);
+                        if (tf == null)
+                            tf = 0.0;
+                        tf++;
+                        X_ij.getHmBoWFeatures().put(str, tf);
+                    }
+
+                    int titleLength1 = titleArr.length;
+
+                    titleArr = dataController.getHmVideo().get(item2).getTitle().split(",");
+                    for (String str:titleArr)
+                    {
+                        Double tf = X_ij.getHmBoWFeatures().get(str);
+                        if (tf == null)
+                            tf = 0.0;
+                        tf--;
+                        X_ij.getHmBoWFeatures().put(str, tf);
+                    }
+
+                    int titleLength2 = titleArr.length;
+                    //X_ij.getHmNumericFeatures().put(4, 1.0 * titleLength1 / titleLength2);
+
+                    // 3. Category
+                    /*
+                    Double tf = X_ij.getHmCategoryFeatures().get(dataController.getHmVideo().get(item1).getCategory());
+                    if (tf == null)
+                        tf = 0.0;
+                    tf++;
+                    X_ij.getHmCategoryFeatures().put(dataController.getHmVideo().get(item1).getCategory(), tf);
+
+                    tf = X_ij.getHmCategoryFeatures().get(dataController.getHmVideo().get(item2).getCategory());
+                    if (tf == null)
+                        tf = 0.0;
+                    tf--;
+                    X_ij.getHmCategoryFeatures().put(dataController.getHmVideo().get(item2).getCategory(), tf);
+                    */
+
+                    // 4. Uploader ID
+                    Double tf = X_ij.getHmChannelIDFeatures().get(dataController.getHmVideo().get(item1).getChannelID());
+                    if (tf == null)
+                        tf = 0.0;
+                    tf++;
+                    X_ij.getHmChannelIDFeatures().put(dataController.getHmVideo().get(item1).getChannelID(), tf);
+
+                    tf = X_ij.getHmChannelIDFeatures().get(dataController.getHmVideo().get(item2).getChannelID());
+                    if (tf == null)
+                        tf = 0.0;
+                    tf--;
+                    X_ij.getHmChannelIDFeatures().put(dataController.getHmVideo().get(item2).getChannelID(), tf);
+
+                    // Compute the <w, X>
+                    Double w = 0.0;
+                    Double exponential = 0.0;
+
+                    for (Integer idF:X_ij.getHmNumericFeatures().keySet())
+                    {
+                        w = modelParams.getOrInitFeature(0, idF);
+                        exponential += w * X_ij.getOrInitFeature(0, idF);
+                    }
+
+                    for (int featureType=1; featureType<4; featureType++)
+                    {
+                        for (String key:X_ij.getStringFeatures(featureType).keySet())
+                        {
+                            w = modelParams.getOrInitFeature(featureType, key);
+                            exponential += w * X_ij.getOrInitFeature(featureType, key);
+                        }
+                    }
+
+                    exponential = Math.exp(exponential);
+                    exponential = exponential / (1 + exponential);
+
+                    //System.out.println(exponential);
+
+                    // Update the parameters
+                    double w_d = 0.0;
+                    for (Integer idF:modelParams.getHmNumericFeatures().keySet())
+                    {
+                        w_d = modelParams.getOrInitFeature(0, idF);
+
+                        if (dataController.getHmVideo().get(item1).getViewCount() >
+                                dataController.getHmVideo().get(item2).getViewCount())
+                            w_d += Configuration.getInstance().getEta() * // learning rate
+                                    X_ij.getOrInitFeature(0, idF) * (- exponential) // gradient
+                                    - Configuration.getInstance().getLambda() * w_d; // regularization
+                        else
+                            w_d += Configuration.getInstance().getEta() * // learning rate
+                                    X_ij.getOrInitFeature(0, idF) * (1 - exponential) // gradient
+                                    - Configuration.getInstance().getLambda() * w_d; // regularization
+
+                        modelParams.setFeature(0, idF, w_d);
+                        //System.out.print(w_d + " ");
+                    }
+
+                    //System.out.println();
+
+                    for (int featureType=1; featureType<4; featureType++)
+                    {
+                        for (String key:modelParams.getStringFeatures(featureType).keySet())
+                        {
+                            w_d = modelParams.getOrInitFeature(featureType, key);
+
+                            if (dataController.getHmVideo().get(item1).getViewCount() >
+                                    dataController.getHmVideo().get(item2).getViewCount())
+                                w_d += Configuration.getInstance().getEta() * // learning rate
+                                       X_ij.getOrInitFeature(featureType, key) * (- exponential)
+                                       - Configuration.getInstance().getLambda() * w_d;
+                            else
+                                w_d += Configuration.getInstance().getEta() * // learning rate
+                                        X_ij.getOrInitFeature(featureType, key) * (1 - exponential)
+                                        - Configuration.getInstance().getLambda() * w_d;
+                            modelParams.setFeature(featureType, key, w_d);
+                            //System.out.print(w_d + " ");
+                        }
+                    }
+                }
+
+            //System.out.println();
+        }
+    }
+
+    void test() throws Exception {
+
+    }
+}
